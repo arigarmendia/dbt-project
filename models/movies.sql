38ab679d67a9c7986fa6584fdeda398924a36bf3
Ariadna Garmendia <arigarmendia@gmail.com>
2025-01-28 18:57:09 -0600

    Cleaned old comments

--- models/movies.sql
+++ models/movies.sql
@@ -1,30 +1,3 @@
--- SELECT  CAST(id as INT) as movie_id, 
---     TO_DATE("Release_Date", 'YY-MM-DD') as release_date,
---     "IMDB_URL" as imdb_url,
---     CAST(CAST("Film_Noir" as INT) as BOOLEAN) as film_noir,
---     CAST(CAST("Action" as INT) as BOOLEAN) as action
--- FROM {{ source('recommmender_system_raw', 'movies') }}
-
--- War
--- Crime
--- Drama
--- Action
--- Comedy
--- Horror
--- Sci_Fi
--- Fantasy
--- Musical
--- Mystery
--- Romance
--- Western
--- Thriller
--- Adventure
--- Animation
--- Film_Noir
--- Children_s
--- Documentary
-
-
 SELECT
     CAST(id AS INT) AS movie_id,
     TO_DATE("Release_Date", 'YY-MM-DD') AS release_date,
